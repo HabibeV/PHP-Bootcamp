3d39345d94fda247282f0871709fd75d873eb352
Habibe <habibe.v720@gmail.com>
2022-11-20 21:56:57 +0300

    Update sil.php

--- Ödev 3/sil.php
+++ Ödev 3/sil.php
@@ -1,11 +1,16 @@
 <?php 
   $id = $_GET["id"]; 
-$baglan= new PDO("mysql:host=localhost;dbname=liste_veri;charset=utf8","root","");
-$baglan->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  require_once "baglanti.php";
 
-
-$sorgu = $baglan->prepare("delete from kisiler where id=?");
-$sil =$sorgu->execute(array($id));
-
-header("Location:listele.php");
+  $sorgu = $baglan->prepare("delete from kisiler where id=?");
+  $sil =$sorgu->execute(array($id));
+  if($sil){
+    header("Location:listele.php");
+  }
+  else{
+    echo "<script>
+    alert('Hata: Kayıt Silinemedi!');
+    window.top.location = 'listele.php';
+    </script>";
+  }
 ?>
