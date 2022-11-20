d101b321d4a6fed8c48a7c7abc40360da871d410
Habibe <habibe.v720@gmail.com>
2022-11-20 21:34:50 +0300

    Create form.php

---
+++ Ödev 3/form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body >
+
+<table   width="35%"  align="center"  >
+    <form action="baglanti.php" method="post"  >
+        <tr >   <td align="center" >Adınız Soyadınız : </td>     </tr>
+        <tr>  <td align="center"> <input type="text" name="adsoyad"></td>   </tr>
+        <tr>  <td align="center"> Telefon Numaranız : </td>   </tr>
+        <tr>   <td align="center"><input type="text" name="numara"></td>   </tr>
+        <tr>   <td align="center"> <input type="submit" value="Bilgileri Kaydet"></td>  </tr>
+    </form>
+</table>
+
+</body>
+</head>
+</html>
